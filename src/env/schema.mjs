a8a1a5d304960a210fee234f4a1e40b693620b1d
Mikey Shean <mdshean2@gmail.com>
2023-01-06 00:55:41 -0800

    try without twilio env vars

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,8 +8,8 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  TWILIO_ACCOUNT_SID: z.string(),
-  TWILIO_AUTH_TOKEN: z.string()
+  // TWILIO_ACCOUNT_SID: z.string(),
+  // TWILIO_AUTH_TOKEN: z.string()
 });
 
 /**
--- src/server/twilio/sendSMS.ts
+++ src/server/twilio/sendSMS.ts
@@ -1,14 +1,14 @@
 import twilio from 'twilio'
 import { env } from "../../env/server.mjs";
 
-const accountSid = env.TWILIO_ACCOUNT_SID
-const authToken = env.TWILIO_AUTH_TOKEN
-const client = twilio(accountSid, authToken)
+// const accountSid = env.TWILIO_ACCOUNT_SID
+// const authToken = env.TWILIO_AUTH_TOKEN
+// const client = twilio(accountSid, authToken)
 
 export const sendSMS = (phoneNumber: string, message: string) => {
-  client.messages.create({
-    body: message,
-    from: '+17262009472',
-    to: phoneNumber
-  })
+  // client.messages.create({
+  //   body: message,
+  //   from: '+17262009472',
+  //   to: phoneNumber
+  // })
 }
