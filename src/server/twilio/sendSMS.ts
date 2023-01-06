7f889bb9f930f4bf7576d91411f0de9e78c0098a
Mikey Shean <mdshean2@gmail.com>
2023-01-06 00:26:39 -0800

    test build

--- src/server/twilio/sendSMS.ts
+++ src/server/twilio/sendSMS.ts
@@ -2,12 +2,12 @@ import twilio from 'twilio'
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
-const client = twilio(accountSid, authToken)
+// const client = twilio(accountSid, authToken)
 
 export const sendSMS = (phoneNumber: string, message: string) => {
-  client.messages.create({
-    body: message,
-    from: '+17262009472',
-    to: phoneNumber
-  })
+  // client.messages.create({
+  //   body: message,
+  //   from: '+17262009472',
+  //   to: phoneNumber
+  // })
 }
