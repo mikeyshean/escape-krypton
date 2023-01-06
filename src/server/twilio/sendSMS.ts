08e75e577047b4285477aa5ae40513d0acc5d82f
Mikey Shean <mdshean2@gmail.com>
2023-01-06 00:48:48 -0800

    try different env getter

--- src/server/twilio/sendSMS.ts
+++ src/server/twilio/sendSMS.ts
@@ -1,7 +1,8 @@
 import twilio from 'twilio'
+import { env } from "../../env/server.mjs";
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID
-const authToken = process.env.TWILIO_AUTH_TOKEN
+const accountSid = env.TWILIO_ACCOUNT_SID
+const authToken = env.TWILIO_AUTH_TOKEN
 const client = twilio(accountSid, authToken)
 
 export const sendSMS = (phoneNumber: string, message: string) => {
